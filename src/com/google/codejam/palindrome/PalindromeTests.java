68a7f9b5573d416150a5b971f7af8f750599bf5e
alex giamas <agiamas@hotmail.com>
2013-04-14 14:43:10 +0300

    removing top level dirs

--- src/com/google/codejam/palindrome/PalindromeTests.java
+++
@@ -1,8 +0,0 @@
-package com.google.codejam.palindrome;
-
-public class PalindromeTests {
-  public static void main(String[] args) {
-    Palindrome pl = new Palindrome("input/palindrome/C-large-1.in");
-    
-  }
-}
